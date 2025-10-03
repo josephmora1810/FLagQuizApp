f3b55e148955991aeb8460040ec73f78441b7582
josephmora1810 <josephmora18102002@gmail.com>
2025-10-03 15:00:56 -0400

    Quiz Page Design

--- app/src/main/java/com/jmora/flagquizapp/view/QuizFragment.kt
+++ app/src/main/java/com/jmora/flagquizapp/view/QuizFragment.kt
@@ -6,13 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.jmora.flagquizapp.R
+import com.jmora.flagquizapp.databinding.FragmentQuizBinding
 
 class QuizFragment : Fragment() {
+
+    lateinit var fragmentQuizBinding : FragmentQuizBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        fragmentQuizBinding = FragmentQuizBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+
+        fragmentQuizBinding.buttonA.setOnClickListener {
+            fragmentQuizBinding.buttonA.text = "Clicked"
+        }
+        fragmentQuizBinding.buttonB.setOnClickListener {
+            fragmentQuizBinding.buttonB.text = "Clicked"
+        }
+        fragmentQuizBinding.buttonC.setOnClickListener {
+            fragmentQuizBinding.buttonC.text = "Clicked"
+        }
+        fragmentQuizBinding.buttonD.setOnClickListener {
+            fragmentQuizBinding.buttonD.text = "Clicked"
+        }
+        fragmentQuizBinding.buttonNext.setOnClickListener {
+            fragmentQuizBinding.buttonNext.text = "Clicked"
+        }
+
+        return fragmentQuizBinding.root
     }
 }
